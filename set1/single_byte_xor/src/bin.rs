89fa9c9aa0b38aea159c9abc1c41d1e86b373a65
Patrick Elsen <pelsen@xfbs.net>
2016-04-30 18:41:02 +0200

    solves challenge 4

--- set1/single_byte_xor/src/bin.rs
+++ set1/single_byte_xor/src/bin.rs
@@ -4,18 +4,39 @@ extern crate single_byte_xor;
 use hex_to_base64::hex;
 use std::env;
 
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
-    if let Some(hex_string) = env::args().nth(1) {
-        if let Some((data, code, sureness)) = try_decrypting(&hex_string) {
-            println!("code: {}", code);
-            println!("sure: {}%", (sureness*100.0)-100.0);
-            println!("data: '{}'", data);
-        } else {
-            println!("couldn't decrypt data :(");
-        }
+    match &env::args().nth(1).unwrap_or(String::new()) as &str {
+        "--hex" => decrypt_hex(&env::args().nth(2).unwrap_or(String::new())),
+        "--file" => decrypt_file(&env::args().nth(2).unwrap_or(String::new())),
+        _ => help()
+    }
+}
+
+fn decrypt_hex(hex_string: &str) {
+    if let Some((data, code, sureness)) = try_decrypting(hex_string) {
+        println!("code: {}", code);
+        println!("sure: {}%", (sureness*100.0)-100.0);
+        println!("data: '{}'", data);
     } else {
-        help();
+        println!("couldn't decrypt data :(");
+    }
+}
+
+fn decrypt_file(file_name: &str) {
+    let mut file = BufReader::new(File::open(file_name).unwrap());
+    let lines: Vec<(u32, String, String, u8, f32)> = (1..).zip(file.lines())
+        .map(|(n, line)| (n, line.unwrap()))
+        .map(|(n, line)| (n, line.clone(), single_byte_xor::decrypt(&hex::decode(&line).unwrap())))
+        .map(|(n, line, result)| (n, line, result.0, result.1, result.2))
+        .collect();
+
+    for (n, line, decrypted, code, sureness) in lines {
+        println!("line #{} was decrypted with code {} and sureness {}%:", n, code, (sureness*100.0)-100.0);
+        println!(" '{}' => '{}'", line, decrypted);
     }
 }
 
