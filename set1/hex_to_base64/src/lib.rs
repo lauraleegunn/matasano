42ecb7c2f9d4bd218a31b9c0e57cea12d2e6f30f
Patrick Elsen <pelsen@xfbs.net>
2016-04-30 12:37:50 +0200

    implements base64_encode

--- set1/hex_to_base64/src/lib.rs
+++ set1/hex_to_base64/src/lib.rs
@@ -109,3 +109,56 @@ fn hex_reencode_works_correctly() {
     assert_eq!(hex_decode(&hex_encode("DeadBeefCafeBabe".as_bytes(), true)).unwrap(), "DeadBeefCafeBabe".as_bytes());
     assert_eq!(hex_decode(&hex_encode("DeadBeefCafeBabe".as_bytes(), false)).unwrap(), "DeadBeefCafeBabe".as_bytes());
 }
+
+fn base64_encode(input: &[u8]) -> String {
+    let input: Vec<u8> = Vec::from(input);
+    return String::from_utf8(input.chunks(3)
+        .map(|chunk|
+             if chunk.len() > 2 {
+                 vec![chunk[0] >> 2, 
+                  ((chunk[0] << 4) | (chunk[1] >> 4)) & ((1 << 6)-1), 
+                  ((chunk[1] << 2) | (chunk[2] >> 6)) & ((1 << 6)-1), 
+                  chunk[2] & ((1 << 6) -1)]
+             } else if chunk.len() > 1 {
+                 vec![chunk[0] >> 2, 
+                  ((chunk[0] << 4) | (chunk[1] >> 4)) & ((1 << 6)-1), 
+                  (chunk[1] << 2) & ((1 << 6)-1), 
+                  64]
+             } else {
+                 vec![chunk[0] >> 2, 
+                  (chunk[0] << 4) & ((1 << 6)-1), 
+                  64,
+                  64]
+             })
+        .flat_map(|e| e.into_iter())
+        .map(|c| match c {
+            alph @ 0 ... 25 => ('A' as u8) + alph,
+            alph @ 26 ... 51 => ('a' as u8) + alph - 26,
+            num  @ 52 ... 61 => ('0' as u8) + num - 52,
+            62 => '+' as u8,
+            63 => '/' as u8,
+            _ => '=' as u8
+        })
+        .collect())
+        .unwrap();
+}
+
+#[test]
+fn base64_encode_works_correctly_on_small_input() {
+    assert_eq!(base64_encode("M".as_bytes()), "TQ==");
+    assert_eq!(base64_encode("Ma".as_bytes()), "TWE=");
+}
+
+#[test]
+fn base64_encode_padding_with_equal_signs_works() {
+    assert_eq!(base64_encode("pleasure.".as_bytes()), "cGxlYXN1cmUu");
+    assert_eq!(base64_encode("leasure.".as_bytes()), "bGVhc3VyZS4=");
+    assert_eq!(base64_encode("easure.".as_bytes()), "ZWFzdXJlLg==");
+    assert_eq!(base64_encode("asure.".as_bytes()), "YXN1cmUu");
+    assert_eq!(base64_encode("sure.".as_bytes()), "c3VyZS4=");
+}
+
+
+fn base64_decode(input: &str) -> Vec<u8> {
+    return Vec::new();
+}
